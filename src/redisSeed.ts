cfa7e66135f22b423284f1f78c73392d56a6be54
KrishikaVansh <krishikavansh20@gmail.com>
2025-12-23 13:53:04 +0530

    nothing changed

--- src/redisSeed.ts
+++ src/redisSeed.ts
@@ -1,34 +1,3 @@
-// import {redisClient} from "./config/redisClient";
-// import { hashApiKey } from "./utils/hash";
-// import dotenv from "dotenv";
-// dotenv.config();
-// async function seed() {
-//   const studentKey = "sk_student_123";
-//   const adminKey = "sk_admin_456";
-//   const studentHash =  hashApiKey(studentKey);
-//   const adminHash =  hashApiKey(adminKey);
-//     console.log(studentHash)
-//   await redisClient.set(`apikey:store:${studentHash}`, JSON.stringify({
-//     owner: "student-service",
-//     roles: ["read"],
-//     isActive: true
-//   }));
-//   await redisClient.set(`apikey:store:${adminHash}`, JSON.stringify({
-//     owner: "admin-service",
-//     roles: ["read", "write"],
-//     isActive: true
-//   }));
-//   // Optional invalid key cache
-//   const invalidKey = "sk_invalid_999";
-//   const invalidHash =  hashApiKey(invalidKey);
-//   await redisClient.set(`apikey:cache:invalid:${invalidHash}`, "1");
-
-//   console.log("Keys inserted");
-//   process.exit(0);
-// }
-
-// seed();
-
 import { redisClient } from "./config/redisClient";
 import { hashApiKey } from "./utils/hash";
 import dotenv from "dotenv";
@@ -37,7 +6,7 @@ dotenv.config();
 
 async function seed() {
   try {
-    // 🔥 REQUIRED in Redis v4+
+    //  REQUIRED in Redis v4+
     if (!redisClient.isOpen) {
       await redisClient.connect();
     }
@@ -74,11 +43,11 @@ async function seed() {
       "1"
     );
 
-    console.log("✅ Keys inserted");
+    console.log("Keys inserted");
   } catch (err) {
-    console.error("❌ Seed failed:", err);
+    console.error(" Seed failed:", err);
   } finally {
-    // ✅ Graceful shutdown
+    //  Graceful shutdown
     await redisClient.quit();
   }
 }
--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -6,6 +6,7 @@ declare global {
       apiKey?: {
         roles: string[];
         owner: string;
+        
       };
     }
   }
